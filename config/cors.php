25d1ee0293e3522990c4b4cc811c5ede843bba1f
Anil <mr.anilbambhaniya@gmail.com>
2026-02-21 00:32:26 +0530

    feat: Add Render deployment URL to allowed CORS origins.

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000', 'http://127.0.0.1:3000'],
+    'allowed_origins' => ['http://localhost:3000', 'http://127.0.0.1:3000', 'https://gruhnirman-crm-backend-0ovx.onrender.com'],
 
     'allowed_headers' => ['*'],
 
