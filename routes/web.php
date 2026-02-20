f80f306db89251abd162eada350d981c70455a70
Anil <mr.anilbambhaniya@gmail.com>
2026-02-20 23:56:09 +0530

    feat: Add web routes to trigger database migrations and seeding via Artisan commands.

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/run-migration', function () {
+    Artisan::call('migrate --force');
+    return "Migrations finished successfully!";
+});
+
+Route::get('/run-seed', function () {
+    Artisan::call('db:seed --force');
+    return "Seed finished successfully!";
+});
 
 Route::get('cache-clear', function () {
     Artisan::call('optimize:clear');
